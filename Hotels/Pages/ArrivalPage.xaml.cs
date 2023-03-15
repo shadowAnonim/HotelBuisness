b989d556874f301cfcd0f6d3a5d6bd8e27a7e9ec
office <office@LAPTOP-AKTTJUBB>
2023-03-15 18:26:01 +0300

    Arrivals

--- Hotels/Pages/ArrivalPage.xaml.cs
+++ Hotels/Pages/ArrivalPage.xaml.cs
@@ -36,7 +36,13 @@ namespace Hotels.Pages
             else
             {
                 this.arrive = new Arrive();
+                this.arrive.Date = DateTime.Now;
+                this.arrive.Room = Utils.db.Bookings.ToList().First().Room;
+                this.arrive.DepartureDate = Utils.db.Bookings.ToList().First().DepartureDate;
+                this.arrive.Booking = Utils.db.Bookings.ToList().First();
+                bookingCb.SelectedIndex = 0;
                 hotelCb.SelectedIndex = 0;
+                roomCb.SelectedIndex = 0;
             }
 
             main.DataContext = this.arrive;
@@ -45,18 +51,29 @@ namespace Hotels.Pages
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             bookingCb.ItemsSource = Utils.db.Bookings.Include(b => b.Room).ThenInclude(r => r.Hotel).ToList();
-        }
-
-        private void hotelCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
             hotelCb.ItemsSource = Utils.db.Hotels.ToList();
             Hotel currHotel = hotelCb.SelectedItem as Hotel;
             roomCb.ItemsSource = Utils.db.Rooms.Where(r => r.Hotel == currHotel).ToList();
         }
 
+        private void hotelCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            roomCb.ItemsSource = Utils.db.Rooms.Include(r => r.Hotel).Where(r => r.Hotel == hotelCb.SelectedItem as Hotel).ToList();
+            roomCb.SelectedIndex = 0;
+        }
+
         private void saveBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (!edit) Utils.db.Arrives.Add(arrive);
+            Utils.db.SaveChanges();
+            NavigationService.GoBack();
+        }
+
+        private void bookingCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Booking b = bookingCb.SelectedItem as Booking;
+            this.arrive.Room = b.Room;
+            this.arrive.DepartureDate = b.DepartureDate;
         }
     }
 }
