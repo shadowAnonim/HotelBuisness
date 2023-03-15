6bdacfe5c23147117fd9b3c7898c646137ce2f28
office <office@LAPTOP-AKTTJUBB>
2023-03-15 14:11:01 +0300

    db changes

---
+++ Hotels/Pages/GuestsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Hotels.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotels.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для GuestsPage.xaml
+    /// </summary>
+    public partial class GuestsPage : Page
+    {
+        public GuestsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void fillDataGrid()
+        {
+            
+
+            
+        }
+    }
+}
