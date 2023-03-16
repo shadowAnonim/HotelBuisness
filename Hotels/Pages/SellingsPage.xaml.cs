6ecea55b198c4ad7bef24edead44b02e58e4c5ca
office <office@LAPTOP-AKTTJUBB>
2023-03-16 15:20:03 +0300

    sellings

---
+++ Hotels/Pages/SellingsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotels.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для SellingsPage.xaml
+    /// </summary>
+    public partial class SellingsPage : Page
+    {
+        public SellingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            datePk.SelectedDate = DateTime.Now;
+            hotelsCb.ItemsSource = Utils.db.Hotels.ToList();
+        }
+    }
+}
