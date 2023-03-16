5ad1d7bdcfe663f6221c9fdcc1caaf85f1013d57
office <office@LAPTOP-AKTTJUBB>
2023-03-16 10:14:24 +0300

    types

--- Hotels/Data/Booking.cs
+++ Hotels/Data/Booking.cs
@@ -7,19 +7,19 @@ public partial class Booking
 {
     public long Id { get; set; }
 
-    public byte[] ArrivalDate { get; set; } = null!;
+    public DateTime? ArrivalDate { get; set; } = null!;
 
-    public byte[] DepartureDate { get; set; } = null!;
+    public DateTime? DepartureDate { get; set; } = null!;
 
     public long RoomId { get; set; }
 
     public long ClientId { get; set; }
 
-    public byte[] BookingDate { get; set; } = null!;
+    public DateTime? BookingDate { get; set; } = null!;
 
-    public byte[] Total { get; set; } = null!;
+    public decimal? Total { get; set; } = null!;
 
-    public byte[] Accept { get; set; } = null!;
+    public bool? Accept { get; set; } = null!;
 
     public virtual ICollection<Arrive> Arrives { get; } = new List<Arrive>();
 
--- Hotels/Data/HotelsContext.cs
+++ Hotels/Data/HotelsContext.cs
@@ -263,7 +263,21 @@ public partial class HotelsContext : DbContext
 
             entity.Property(e => e.Name).HasColumnType("VARCHAR (2, 15)");
         });
+        modelBuilder
+        .Entity<Booking>()
+        .Property(e => e.Total)
+        .HasConversion<decimal>();
 
+        modelBuilder
+        .Entity<Booking>()
+        .Property(e => e.Accept)
+        .HasConversion<bool>();
+        OnModelCreatingPartial(modelBuilder);
+
+        modelBuilder
+        .Entity<RoomPrice>()
+        .Property(e => e.Price)
+        .HasConversion<decimal>();
         OnModelCreatingPartial(modelBuilder);
     }
 
