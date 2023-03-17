e09c817ca13167c48b36fa17fc8e3b385436d05c
office <office@LAPTOP-AKTTJUBB>
2023-03-17 11:05:54 +0300

    Request Info

--- Hotels/Pages/CleaningRequestPage.xaml.cs
+++ Hotels/Pages/CleaningRequestPage.xaml.cs
@@ -30,6 +30,15 @@ namespace Hotels.Pages
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Room room = (((sender as Button).Parent as StackPanel).Parent as StackPanel).DataContext as Room;
+            if (Utils.db.Cleans.Where(c => c.Room == room && c.CleanStateId == 1).ToList().Count > 0)
+            {
+                Utils.Error("В этом номере уже есть активная заявка на уборку");
+                return;
+            }
+            Clean clean = new Clean() { Date = DateTime.Now, Room = room, CleanStateId = 1};
+            Utils.db.Add(clean);
+            Utils.db.SaveChanges();
+            MessageBox.Show($"Создана заяка на уборку номера {room.Name} в отеле {room.Hotel.Name}", "Новая заявка", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -41,7 +50,8 @@ namespace Hotels.Pages
         private void hotelCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Hotel hotel = hotelCb.SelectedItem as Hotel;
-            roomsList.ItemsSource = Utils.db.Rooms.Where(r => r.Hotel == hotel).ToList();            
+            roomsList.ItemsSource = Utils.db.Rooms.Where(r => r.Hotel == hotel && 
+                (r.StateId == 1 || r.StateId == 4)).ToList();            
         }
     }
 }
